11027d8e447e3299dd9d9a60c5d1f11bff1b2aea
Bruna Terribile <brunaterribile@gmail.com>
2023-04-03 14:12:00 -0300

    feat: doctor + patient schema and md validation

--- src/routes/doctorRoutes.js
+++ src/routes/doctorRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import doctorControllers from "../controllers/doctorControllers.js";
+import { validateSchema } from "../middlewares/schemaValidationMiddleware.js";
+import { doctorSchema } from "../schemas/Doctor.js";
 
 const doctorRoutes = Router();
 
-doctorRoutes.post('/', doctorControllers.create)
+doctorRoutes.post('/', validateSchema(doctorSchema), doctorControllers.create)
 //doctorRoutes.get('/', doctorController.get)
 //doctorRoutes.get('/:id', doctorController.put)
 
